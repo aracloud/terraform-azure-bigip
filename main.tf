9bb90b73697af65fcffeddb14c811ceec6e1eae2
aracloud <aracloud@gmx.net>
2025-12-18 11:18:31 +0100

    change

--- main.tf
+++ main.tf
@@ -3,11 +3,9 @@ terraform {
   required_providers {
     azapi = {
       source  = "azure/azapi"
-      version = "~>1.5"
     }
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.0"
     }
   }
 }
